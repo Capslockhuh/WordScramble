47a5887f3107806dca4f276e10a6a9bb7fa9b3b5
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-06-05 17:40:54 +0200

    Added a function that adds the typed word to the usedWords array.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,6 +12,18 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    // Adds the typed word to the usedWords array.
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences.
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Exit if the remaining string is empty.
+        guard answer.count > 0 else { return }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
     var body: some View {
         NavigationView {
             List {
