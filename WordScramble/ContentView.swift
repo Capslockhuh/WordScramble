e292b82fcdbd6b148f33ba52e7e2d52e1135fee2
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-18 10:53:59 +0200

    Improved support for VoiceOver

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -120,6 +120,8 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityLabel("\(word), \(word.count) letters")
                     }
                 }
             }
